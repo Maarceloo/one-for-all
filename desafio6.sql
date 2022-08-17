419f5a7ac88eae6cbfa9caf7a9e5541d830b7b94
Marcelo De Lima <delima_marcelo@outlook.com>
2022-08-17 19:54:11 -0300

    FEAT Desafio 6 Finalizado

---
+++ desafio6.sql
@@ -0,0 +1,9 @@
+SELECT 
+	ROUND(MIN(p.valor), 2) AS faturamento_minimo,
+    ROUND(MAX(p.valor), 2) AS faturamento_maximo,
+    ROUND(AVG(p.valor), 2)AS faturamento_medio,
+    ROUND(SUM(p.valor), 2) AS faturamento_total
+FROM 
+	SpotifyClone.plano AS p
+JOIN 
+	SpotifyClone.usuario AS u ON u.plano_id = p.plano_id;
